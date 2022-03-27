bc332704f8b207e1ad220409fe801c48edbf2f19
Ray Huang <47706097+ExplorerRay@users.noreply.github.com>
2022-03-27 08:58:55 +0800

    Add files via upload

--- pa1.cpp
+++ pa1.cpp
@@ -106,7 +106,7 @@ public:
         cout << "average takeoff waiting time: " << (double)stat.wt_tkoff_tm/tk_total << '\n';
         cout << "average fuel saved: " << (double)stat.time_saved/stat.ald_land << '\n';
         cout << "total plane in emergency: " << stat.eg_plane << '\n';
-        cout << "total plane crashed: " << stat.crash_plane;
+        cout << "total plane crashed: " << stat.crash_plane << '\n';
     }
 
     void run(int step){
@@ -136,7 +136,7 @@ public:
                     else rw4.land_queue2.emplace_back(make_pair(land_id,fuel_lv));
                     dc_rwqe++;
                 }
-                //可能可再改 注意tt_plane及else部分
+                //可能可再改 注意else部分
 
                 land_id+=2;
             }
@@ -222,115 +222,85 @@ public:
             if(eg_queue.size() > 4)stat.crash_plane += eg_queue.size()-4;
             for(int i=0;i<(int)eg_queue.size();i++){
                 if(i>=4)break;
-                if(i==0)rw1.id_now = eg_queue[i];
+                if(i==0){rw1.id_now = eg_queue[i];stat.ald_tkoff++;}
                 else{
-                    if((dc_rwqe+i-1)%3 == 0)rw2.id_now = eg_queue[i];
-                    else if((dc_rwqe+i-1)%3 == 1)rw3.id_now = eg_queue[i];
-                    else rw4.id_now = eg_queue[i];
+                    if((dc_rwqe+i-1)%3 == 0){rw2.id_now = eg_queue[i];stat.ald_tkoff++;}
+                    else if((dc_rwqe+i-1)%3 == 1){rw3.id_now = eg_queue[i];stat.ald_tkoff++;}
+                    else {rw4.id_now = eg_queue[i];stat.ald_tkoff++;}
                 }
             }
-            stat.ald_tkoff += min((int)eg_queue.size(),4);
+            //stat.ald_tkoff += min((int)eg_queue.size(),4);
             eg_queue.clear();
             
             if(rw1.id_now == -1){
-                if(!rw1.tkoff_queue.empty()){
+                if(rw1.tkoff_queue.size()!=0){
                     rw1.id_now = rw1.tkoff_queue[0];
                     stat.ald_tkoff++;
                     //pop front?
-                    for(int i=0;i<(int)rw1.tkoff_queue.size()-1;i++){
-                        rw1.tkoff_queue[i]=rw1.tkoff_queue[i+1];
-                    }
-                    rw1.tkoff_queue.pop_back();
+                    rw1.tkoff_queue.erase(rw1.tkoff_queue.begin());
                 }
             }
             if(rw2.id_now == -1){
-                if(!rw2.land_queue1.empty()){
+                if(rw2.land_queue1.size()!=0){
                     rw2.id_now = rw2.land_queue1[0].first;
                     stat.time_saved += rw2.land_queue1[0].second;
                     stat.ald_land++;
-                    for(int i=0;i<(int)rw2.land_queue1.size()-1;i++){
-                        rw2.land_queue1[i]=rw2.land_queue1[i+1];
-                    }
-                    rw2.land_queue1.pop_back();
+                    rw2.land_queue1.erase(rw2.land_queue1.begin());
                 }
-                else if(!rw2.land_queue2.empty()){
+                else if(rw2.land_queue2.size()!=0){
                     rw2.id_now = rw2.land_queue2[0].first;
                     stat.time_saved += rw2.land_queue2[0].second;
                     stat.ald_land++;
-                    for(int i=0;i<(int)rw2.land_queue2.size()-1;i++){
-                        rw2.land_queue2[i]=rw2.land_queue2[i+1];
-                    }
-                    rw2.land_queue2.pop_back();
+                    rw2.land_queue2.erase(rw2.land_queue2.begin());
                 }
-                else if(!rw2.tkoff_queue.empty()){
+                else if(rw2.tkoff_queue.size()!=0){
                     rw2.id_now = rw2.tkoff_queue[0];
                     stat.ald_tkoff++;
-                    for(int i=0;i<(int)rw2.tkoff_queue.size()-1;i++){
-                        rw2.tkoff_queue[i]=rw2.tkoff_queue[i+1];    
-                    }
-                    rw2.tkoff_queue.pop_back();
+                    rw2.tkoff_queue.erase(rw2.tkoff_queue.begin());
                 }
             }
             if(rw3.id_now == -1){
-                if(!rw3.land_queue1.empty()){
+                if(rw3.land_queue1.size()!=0){
                     rw3.id_now = rw3.land_queue1[0].first;
                     stat.time_saved += rw3.land_queue1[0].second;
                     stat.ald_land++;
-                    for(int i=0;i<(int)rw3.land_queue1.size()-1;i++){
-                        rw3.land_queue1[i]=rw3.land_queue1[i+1];
-                    }
-                    rw3.land_queue1.pop_back();
+                    rw3.land_queue1.erase(rw3.land_queue1.begin());
                 }
-                else if(!rw3.land_queue2.empty()){
+                else if(rw3.land_queue2.size()!=0){
                     rw3.id_now = rw3.land_queue2[0].first;
                     stat.time_saved += rw3.land_queue2[0].second;
                     stat.ald_land++;
-                    for(int i=0;i<(int)rw3.land_queue2.size()-1;i++){
-                        rw3.land_queue2[i]=rw3.land_queue2[i+1];
-                    }
-                    rw3.land_queue2.pop_back();
+                    rw3.land_queue2.erase(rw3.land_queue2.begin());
                 }
-                else if(!rw3.tkoff_queue.empty()){
+                else if(rw3.tkoff_queue.size()!=0){
                     rw3.id_now = rw3.tkoff_queue[0];
                     stat.ald_tkoff++;
-                    for(int i=0;i<(int)rw3.tkoff_queue.size()-1;i++){
-                        rw3.tkoff_queue[i]=rw3.tkoff_queue[i+1];
-                    }
-                    rw3.tkoff_queue.pop_back();
+                    rw3.tkoff_queue.erase(rw3.tkoff_queue.begin());
                 }
             }
             if(rw4.id_now == -1){
-                if(!rw4.land_queue1.empty()){
+                if(rw4.land_queue1.size()!=0){
                     rw4.id_now = rw4.land_queue1[0].first;
                     stat.time_saved += rw4.land_queue1[0].second;
                     stat.ald_land++;
-                    for(int i=0;i<(int)rw4.land_queue1.size()-1;i++){
-                        rw4.land_queue1[i]=rw4.land_queue1[i+1];
-                    }
-                    rw4.land_queue1.pop_back();
+                    rw4.land_queue1.erase(rw4.land_queue1.begin());
                 }
-                else if(!rw4.land_queue2.empty()){
+                else if(rw4.land_queue2.size()!=0){
                     rw4.id_now = rw4.land_queue2[0].first;
                     stat.time_saved += rw4.land_queue2[0].second;
                     stat.ald_land++;
-                    for(int i=0;i<(int)rw4.land_queue2.size()-1;i++){
-                        rw4.land_queue2[i]=rw4.land_queue2[i+1];
-                    }
-                    rw4.land_queue2.pop_back();
+                    rw4.land_queue2.erase(rw4.land_queue2.begin());
                 }
-                else if(!rw4.tkoff_queue.empty()){
+                else if(rw4.tkoff_queue.size()!=0){
                     rw4.id_now = rw4.tkoff_queue[0];
                     stat.ald_tkoff++;
-                    for(int i=0;i<(int)rw4.tkoff_queue.size()-1;i++){
-                        rw4.tkoff_queue[i]=rw4.tkoff_queue[i+1];
-                    }
-                    rw4.tkoff_queue.pop_back();
+                    rw4.tkoff_queue.erase(rw4.tkoff_queue.begin());
                 }
             }
             stat.wt_land_tm = stat.wt_land_tm + land_total - stat.ald_land;
             stat.wt_tkoff_tm = stat.wt_tkoff_tm + tk_total - stat.ald_tkoff;
 
-            //fuel level decrease 
+            //fuel level decrease and delete planes whose fuel lv=0
             for(int i=0;i<(int)rw2.land_queue1.size();i++){
                 //if(rw2.land_queue1[i].first != rw2.id_now)
                 if(rw2.land_queue1[i].second == 0)rw2.land_queue1.erase(rw2.land_queue1.begin()+i);
@@ -376,6 +346,7 @@ int main(){
 
     airport ap;
     int simu_time=500;
+    bool b=true;
     cout << "How many time unit you want to simulate: " << simu_time << '\n';
     for(int t=1;t<=simu_time;t++){
         ap.i_output(t);
@@ -383,7 +354,11 @@ int main(){
         ap.run(2);
         ap.run(3);
         ap.run(4);
+        if(ap.tk_total < ap.stat.ald_tkoff){
+            b=false;
+        }
     }
     cout << "How many time unit you simulate: " << simu_time << '\n';
     ap.print_statinfo();
+    if(!b)cout << "WTF!!!\n";
 }
