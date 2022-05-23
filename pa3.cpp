6cecb8bedd74406a4907931f5faaf44258d7a37b
Ray Huang <47706097+ExplorerRay@users.noreply.github.com>
2022-05-23 12:13:50 +0800

    Add files via upload

---
+++ pa3.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void insertionSort(vector<int> arr){
+
+}
+void quickSort(vector<int> arr){
+
+}
+void radixSort(vector<int> arr){
+
+}
+
+int main(){
+    cout << "DS-OO Program3-Demo\n";
+    srand(time(NULL));
+
+    int inp;
+    cin >> inp;
+    if(inp<=0){//part 1
+        vector<int> v = {168,179,208,306,93,859,984,55,9,271,33};
+
+    }
+    else{
+        vector<int> v(inp);
+        for(int t=0;t<inp;t++)v[t] = rand()%999 +1;
+
+        if(inp<=100){//format 1
+
+        }
+        else{
+
+        }
+    }
+}
