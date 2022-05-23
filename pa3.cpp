5874123aa5bcd7e7ef5d4c1ed1b4f4977e698b39
Ray Huang <47706097+ExplorerRay@users.noreply.github.com>
2022-05-23 19:29:39 +0800

    Add files via upload
    quick radix not yet

--- pa3.cpp
+++ pa3.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
-void insertionSort(vector<int> arr){
+vector<int> insertionSort(vector<int> arr){
+    int sz = arr.size();
+    for(int i=1;i<sz;i++){
+        for(int j=0;j<i;j++){
+            if(arr[i] < arr[j]){
+                int tmp = arr[i];
+                for(int k=i;k>j;k--)arr[k] = arr[k-1];
+                arr[j] = tmp;
+                break;
+            }
+        }
+        if(sz<=100){
+            cout << setw(2) << i << " : ";
+            for(int t=0;t<sz;t++)cout << arr[t] << ',';
+            cout << '\n';
+        }
+    }
 
+    return arr;
 }
 void quickSort(vector<int> arr){
 
@@ -13,13 +30,23 @@ void radixSort(vector<int> arr){
 }
 
 int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+
     cout << "DS-OO Program3-Demo\n";
     srand(time(NULL));
+    double START, END;
 
     int inp;
     cin >> inp;
     if(inp<=0){//part 1
         vector<int> v = {168,179,208,306,93,859,984,55,9,271,33};
+        cout << "Before : ";
+        for(int t=0;t<(int)v.size();t++)cout << v[t] << ',';cout << "\n\n";
+        cout << "Insertion sort:\n";
+        START = clock();
+        vector<int> is = insertionSort(v);//saved for check correction
+        END = clock();
+        cout << "sorting time : " << (END - START) / CLOCKS_PER_SEC << " s\n\n\n";
 
     }
     else{
@@ -27,9 +54,23 @@ int main(){
         for(int t=0;t<inp;t++)v[t] = rand()%999 +1;
 
         if(inp<=100){//format 1
+            cout << "Before : ";
+            for(int t=0;t<(int)v.size();t++)cout << v[t] << ',';cout << "\n\n";
+            cout << "Insertion sort:\n";
+            START = clock();
+            vector<int> is = insertionSort(v);
+            END = clock();
+            cout << "sorting time : " << (END - START) / CLOCKS_PER_SEC << " s\n\n\n";
 
+            //quick
         }
         else{
+            cout << "Insertion sort:\n";
+            START = clock();
+            vector<int> is = insertionSort(v);
+            END = clock();
+            cout << "sorting time : " << (END - START) / CLOCKS_PER_SEC << " s\n\n\n";
+
 
         }
     }
