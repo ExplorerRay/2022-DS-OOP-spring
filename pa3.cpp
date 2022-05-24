40bfd834752440ac7f2e51f4f59cd33c71bc32b5
Ray Huang <47706097+ExplorerRay@users.noreply.github.com>
2022-05-24 12:03:06 +0800

    Add files via upload
    radix sort not yet

--- pa3.cpp
+++ pa3.cpp
@@ -23,14 +23,36 @@ vector<int> insertionSort(vector<int> arr){
     return arr;
 }
 
-int position(vector<int> arr, int l, int r){//return position(index) of pivot
+int position(vector<int> &arr, int l, int r){//return position(index) of pivot
     int pivot = arr[l];
-    int i = l+1;
-}
-vector<int> quickSort(vector<int> arr, int l, int r){
-    
+    int j=l;
+    for(int i=l+1;i<=r;i++){
+        if(pivot >= arr[i]){
+            j++;
+        }
+    }
+    swap(arr[l], arr[j]);
 
-    return arr;
+    while(l<j && r>j){
+        while(arr[l] <= pivot)l++;
+
+        while(arr[r] > pivot)r--;
+
+        if(l<j && r>j)swap(arr[l++],arr[r--]);
+    }
+
+    return j;
+}
+void quickSort(vector<int> &arr, int l, int r){
+    if(l>=r)return;
+    int pos = position(arr, l, r);
+    if(arr.size()<=100){
+        cout << l << '-' << r << " : \n";
+        for(int t=0;t<(int)arr.size();t++){cout << arr[t] << ',';}
+        cout << '\n';
+    }
+    quickSort(arr, l, pos-1);
+    quickSort(arr, pos+1, r);
 }
 void radixSort(vector<int> arr){
 
@@ -48,7 +70,7 @@ int main(){
     if(inp<=0){//part 1
         vector<int> v = {168,179,208,306,93,859,984,55,9,271,33};
         cout << "Before : ";
-        for(int t=0;t<(int)v.size();t++)cout << v[t] << ',';cout << "\n\n";
+        for(int t=0;t<(int)v.size();t++){cout << v[t] << ',';}cout << "\n\n";
         cout << "Insertion sort:\n";
         START = clock();
         vector<int> is = insertionSort(v);//saved for check correction
@@ -58,11 +80,16 @@ int main(){
         //quick
         cout << "Quick sort:\n";
         cout << "Before : ";
-        for(int t=0;t<(int)v.size();t++)cout << v[t] << ',';cout << "\n\n";
+        for(int t=0;t<(int)v.size();t++){cout << v[t] << ',';}cout << "\n\n";
+        vector<int> qs = v;
         START = clock();
-        vector<int> qs = quickSort(v, 0, v.size()-1);
+        quickSort(qs, 0, qs.size()-1);
         END = clock();
-        cout << "sorting time : " << (END - START) / CLOCKS_PER_SEC << " s\n\n\n";
+        cout << "sorting time : " << (END - START) / CLOCKS_PER_SEC << " s\n";
+        if(qs == is)cout << "Correct!!\n\n";
+        else cout << "Not correct\n\n";
+
+        //radix
     }
     else{
         vector<int> v(inp);
@@ -70,7 +97,7 @@ int main(){
 
         if(inp<=100){//format 1
             cout << "Before : ";
-            for(int t=0;t<(int)v.size();t++)cout << v[t] << ',';cout << "\n\n";
+            for(int t=0;t<(int)v.size();t++){cout << v[t] << ',';}cout << "\n\n";
             cout << "Insertion sort:\n";
             START = clock();
             vector<int> is = insertionSort(v);
@@ -80,7 +107,15 @@ int main(){
             //quick
             cout << "Quick sort:\n";
             cout << "Before : ";
-            for(int t=0;t<(int)v.size();t++)cout << v[t] << ',';cout << "\n\n";
+            for(int t=0;t<(int)v.size();t++){cout << v[t] << ',';}cout << "\n\n";
+            vector<int> qs = v;
+            START = clock();
+            quickSort(qs, 0, qs.size()-1);
+            END = clock();
+            cout << "sorting time : " << (END - START) / CLOCKS_PER_SEC << " s\n";
+            if(qs == is)cout << "Correct!!\n\n";
+            else cout << "Not correct\n\n";
+
         }
         else{
             cout << "Insertion sort:\n";
@@ -90,6 +125,13 @@ int main(){
             cout << "sorting time : " << (END - START) / CLOCKS_PER_SEC << " s\n\n\n";
 
             cout << "Quick sort:\n";
+            vector<int> qs = v;
+            START = clock();
+            quickSort(qs, 0, qs.size()-1);
+            END = clock();
+            cout << "sorting time : " << (END - START) / CLOCKS_PER_SEC << " s\n";
+            if(qs == is)cout << "Correct!!\n\n";
+            else cout << "Not correct\n\n";
         }
     }
 }
